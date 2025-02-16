72a1fe13fc7e76569b373aa65e5cadf4a6a95e1b
Kagura <me@kagurach.uk>
2025-02-16 21:57:22 +0800

    Init

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://git.kagurach.uk/kagura/revyos-docs-temp', // FIXME
+            'https://github.com/kagura114/revyos-docs-new', // FIXME
         },
         theme: {
           customCss: './src/css/custom.css',
